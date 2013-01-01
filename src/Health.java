8f1dccdf4fc35f4f8a79aa980ff98e78dd10d5bf
Luke <Luke@Luke-Mac.home>
2013-01-01 21:33:20 +0000

    Removed code that isn't needed

--- src/Health.java
+++ src/Health.java
@@ -14,52 +14,6 @@ public class Health {
 	
 	public Health(){
 		
-		switch(playerHealth){
-		case 100:
-			healthBar.width = 100;
-			playerIsDead = false;
-			break;
-		case 90:
-			healthBar.width = 90;
-			playerIsDead = false;
-			break;
-		case 80:
-			healthBar.width = 80;
-			playerIsDead = false;
-			break;
-		case 70:
-			healthBar.width = 70;
-			playerIsDead = false;
-			break;
-		case 60:
-			healthBar.width = 60;
-			playerIsDead = false;
-			break;
-		case 50:
-			healthBar.width = 50;
-			playerIsDead = false;
-			break;
-		case 40:
-			healthBar.width = 40;
-			playerIsDead = false;
-			break;
-		case 30:
-			healthBar.width = 30;
-			playerIsDead = false;
-			break;
-		case 20:
-			healthBar.width = 20;
-			playerIsDead = false;
-			break;
-		case 10:
-			healthBar.width = 10;
-			playerIsDead = false;
-			break;
-		case 0:
-			healthBar.width = 0;
-			playerIsDead = true;
-			break;
-		}
 	}
 	
 	public void tick(){
