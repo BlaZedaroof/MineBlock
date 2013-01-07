4ee6a0b78b3e7b3e331e11df2f17426ce23be603
Luke <Luke@Luke-Mac.home>
2013-01-07 07:23:34 +0000

    Added Debugging for Grass Spread

--- src/Level.java
+++ src/Level.java
@@ -309,6 +309,7 @@ public class Level {
 					if((block[x][y].id == Tile.earth && block[x][y-1].id == Tile.air) && (block[x-1][y].id == Tile.grass || block[x+1][y].id == Tile.grass || block[x-1][y+1].id == Tile.grass || block[x+1][y+1].id == Tile.grass)){
 						if(rand.nextInt(grassSpreadSpeed) == 1){
 							block[x][y].id = Tile.grass;
+							System.out.println("Grass Spread!");
 						}
 					}
 				}catch(Exception e){}
